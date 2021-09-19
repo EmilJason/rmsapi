8d5bb29767a12511cf11f26bbd2eeb78d541d526
Emil Jason DC. Datuin <emiljasondatuin@gmail.com>
2021-09-19 16:12:05 +0800

    second commit

--- router/person.router.js
+++ router/person.router.js
@@ -2,12 +2,18 @@ const router = require("express").Router();
 
 const data = [];
 
-router.post("/person", (req, res) => {
+//creating new person
+router.post("/person/new", (req, res) => {
   let person = {
     name: req.body.name,
     age: req.body.age,
   };
   data.push(person);
+  res.json(data);
+});
+
+//getting the list of all persons' record
+router.get("/person", (req, res) => {
   res.json(data);
 });
 
